ac3509ddb4a99e82b6105a2e8b8ea943b0b246c6
Andrew Martin <a.a.d.martin@gmail.com>
2014-08-25 22:05:38 +1000

    fixed ticket id

--- zego/tickets.go
+++ zego/tickets.go
@@ -18,7 +18,7 @@ func GetTicket(auth Auth, ticket_id string) *Resource {
 
 }
 
-func GetMultipleTickets(auth Auth) *Resource {
+func GetMultipleTickets(auth Auth, ticket_id string) *Resource {
 
 	path := auth.Subdomain + "/api/v2/" + "/tickets/" + ticket_id + ".json"
 	resource := api(auth, "GET", path, "")
