9f8b4a665a5c43dd42a82374cb8be9406a2a5fae
Andrew Martin <a.a.d.martin@gmail.com>
2014-09-03 23:31:04 +1000

    Added ticket struct

--- zego/api.go
+++ zego/api.go
@@ -8,6 +8,34 @@ import (
 )
 
 
+type Ticket struct {
+  Id                 uint32
+  Url                string
+  External_id        string
+  Created_at         time.Time
+  Updated_at         time.Time
+  Ttype              string
+  Subject            string
+  Raw_subject        string
+  Description        string
+  Priority           string
+  Status             string
+  Recipient          string
+  Requester_id       uint32
+  Submitter_id       uint32
+  Assignee_id        uint32
+  Organization_id    uint32
+  Group_id           uint32
+  Collaborator_ids   []uint32
+  Forum_topic_id     uint32
+  Problem_id         uint32
+  Has_incidents      bool
+  Due_at             time.Time
+  Tags               []string
+  Via                interface{}
+}
+
+
 type Connector interface {
     ListViews() *Resource
 }
