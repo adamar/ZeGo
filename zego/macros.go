9d0a6d26f98c6633edea93850f097c5b5b1fda0a
Andrew Martin <a.a.d.martin@gmail.com>
2014-09-29 21:03:09 +1000

    updated macros

--- zego/macros.go
+++ zego/macros.go
@@ -1,17 +1,17 @@
 package zego
 
-func ListMacros(auth Auth) *Resource {
+func (a Auth) ListMacros() *Resource {
 
-	path := "/api/v2/macros.json"
+	path := "/macros.json"
 	resource := api(auth, "GET", path, "")
 
 	return resource
 
 }
 
-func ApplyMacro(auth Auth, ticket_id string, macro_id string) *Resource {
+func (a Auth) ApplyMacro(ticket_id string, macro_id string) *Resource {
 
-	path := "/api/v2/" + "/tickets/" + ticket_id + "/macros/" + macro_id + "/apply.json"
+	path := "/tickets/" + ticket_id + "/macros/" + macro_id + "/apply.json"
 	resource := api(auth, "GET", path, "")
 
 	return resource
