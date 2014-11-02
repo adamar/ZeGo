6d008c31c1d3f292390c886a0b66c170c5c204e2
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-02 17:21:54 +1100

    move ticket struct

--- zego/tickets.go
+++ zego/tickets.go
@@ -1,5 +1,45 @@
 package zego
 
+type TicketArray struct {
+        Count         int
+        Created       string //time.Time
+        Next_page     string
+        Previous_page string
+        Tickets       []Ticket
+}
+
+type Ticket struct {
+        Id                    uint32
+        Url                   string
+        External_id           string
+        Created_at            string //time.Time
+        Updated_at            string //time.Time
+        Type                  string
+        Subject               string
+        Raw_subject           string
+        Description           string
+        Priority              string
+        Status                string
+        Recipient             string
+        Requester_id          uint32
+        Submitter_id          uint32
+        Assignee_id           uint32
+        Organization_id       uint32
+        Group_id              uint32
+        Collaborator_ids      []uint32
+        Forum_topic_id        uint32
+        Problem_id            uint32
+        Has_incidents         bool
+        Due_at                string //time.Time
+        Tags                  []string
+        Satisfaction_rating   string
+        Ticket_form_id        uint32
+        Sharing_agreement_ids interface{}
+        Via                   interface{}
+        Custom_Fields         interface{}
+        Fields                interface{}
+}
+
 func (a Auth) ListTickets() (*Resource, error) {
 
 	path := "/tickets.json"
