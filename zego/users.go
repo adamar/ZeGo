663ca26c45d583db851419a8e6a53fe1c4f596f5
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-02 17:07:16 +1100

    Adding user struct

--- zego/users.go
+++ zego/users.go
@@ -1,5 +1,41 @@
 package zego
 
+type User struct {
+        Id                    uint32
+        Url                   string
+        Name                  string
+        External_id
+        Alias
+        Created_at
+        Updated_at
+        Active
+        Verified
+        Shared
+        Shared_agent
+        Locale
+        Locale_id
+        Time_zone
+        Last_login_at
+        Email
+        Phone
+        Signature
+        Details
+        Notes
+        Organization_id
+        Role
+        Customer_role_id
+        Moderator
+        Ticket_restriction
+        Only_private_comments
+        Tags
+        Restricted_agent
+        Suspended
+        Photo
+        User_fields
+}
+
+
+
 func (a Auth) ListUsers() (*Resource, error) {
 
 	path := "/users.json"
