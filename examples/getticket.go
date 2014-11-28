23b060576c531f34a1550fe720555dc6847510b0
arran ubels <a.ubels@base2services.com>
2014-11-28 15:17:27 +1100

    Bug fixes

--- examples/getticket.go
+++ examples/getticket.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/adamar/zego"
+	"github.com/adamar/zego/zego"
 	"log"
+	"encoding/json"
 )
 
 func main() {
-
-	auth := zego.Auth{"user@example.com", "password", "subdomain.zendesk.com"}
-        response := auth.ListTickets()
-        tickets := &zego.TicketArray{}
-        json.Unmarshal([]byte(response.Raw), tickets)
+	auth := zego.Auth{"user@example.com", "Password", "example.zendesk.com"}
+	// auth := zego.Auth{"user@example.com/token", "Token", "example.zendesk.com"}
+	response, _ := auth.ListTickets()
+	tickets := &zego.TicketArray{}
+	json.Unmarshal([]byte(response.Raw), tickets)
 	log.Print(tickets)
-
 }
