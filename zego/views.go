31d14e6bd1b9fe18b22f8652edb36e9820568381
Andrew Martin <a.a.d.martin@gmail.com>
2014-12-08 17:37:07 +1100

    Added views struct

--- zego/views.go
+++ zego/views.go
@@ -1,5 +1,24 @@
 package zego
 
+
+View struct {
+    Id           int `json:"id"`
+    Title        string `json:"title"`
+    Active       bool `json:"active"`
+    SlaId        int `json:"sla_id"`
+    Execution    []interface{} `json:"execution"`
+    Conditions   []interface{} `json:"conditions"`
+    Restriction  *Restriction `json:"restriction"`
+}
+
+
+Restriction  struct {
+    Type         string `json:"type"`
+    Id           int `json:"id"`
+} 
+
+
+
 func (a Auth) ListViews() (*Resource, error) {
 
 	path := "/views.json"
