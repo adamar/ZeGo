6c27f410e3caf988479673d9c405167b43d2a3b3
Andrew Martin <a.a.d.martin@gmail.com>
2014-12-09 11:34:50 +1100

    Added search structs

---
+++ zego/search.go
@@ -0,0 +1,28 @@
+
+package zego
+
+
+
+
+type Search_Results struct {
+    Count            int `json:"count"`
+    NextPage         string `json:"next_page"`
+    PrevPage         string `json:"prev_page"`
+    Results          []*Result `json:"results"`
+}
+
+
+type Result struct {
+    Name             string `json:"name"`
+    CreatedAt        string `json:"created_at"`
+    UpdatedAt        string `json:"updated_at"`
+    Id               int `json:"id"`
+    ResultType       string `json:"result_type"`
+    Url              string `json:"url"`
+}
+
+
+
+
+
+
