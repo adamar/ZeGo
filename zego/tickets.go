f2b34b83f068333785d65462c5bdb5f06c1c841c
Andrew Martin <a.a.d.martin@gmail.com>
2015-03-24 11:17:41 +1100

    Added json fields for tickets

--- zego/tickets.go
+++ zego/tickets.go
@@ -1,43 +1,43 @@
 package zego
 
 type TicketArray struct {
-	Count         int
-	Created       string //time.Time
-	Next_page     string
-	Previous_page string
+	Count         int    `json:"count"`
+	Created       string `json:"created"`
+	Next_page     string `json:"next_page"`
+	Previous_page string `json:"previous_page"`
 	Tickets       []Ticket
 }
 
 type Ticket struct {
-	Id                    uint32
-	Url                   string
-	External_id           string
-	Created_at            string //time.Time
-	Updated_at            string //time.Time
-	Type                  string
-	Subject               string
-	Raw_subject           string
-	Description           string
-	Priority              string
-	Status                string
-	Recipient             string
-	Requester_id          uint32
-	Submitter_id          uint32
-	Assignee_id           uint32
-	Organization_id       uint32
-	Group_id              uint32
-	Collaborator_ids      []uint32
-	Forum_topic_id        uint32
-	Problem_id            uint32
-	Has_incidents         bool
-	Due_at                string //time.Time
-	Tags                  []string
-	Satisfaction_rating   string
-	Ticket_form_id        uint32
-	Sharing_agreement_ids interface{}
-	Via                   interface{}
-	Custom_Fields         interface{}
-	Fields                interface{}
+	Id                    uint32      `json:"id"`
+	URL                   string      `json:"url"`
+	ExternalId            string      `json:"external_id"`
+	CreatedAt             string      `json:"created_at"`
+	UpdatedAt             string      `json:"updated_at"`
+	Type                  string      `json:"type"`
+	Subject               string      `json:"subject"`
+	RawSubject            string      `json:"raw_subject"`
+	Description           string      `json:"description"`
+	Priority              string      `json:"priority"`
+	Status                string      `json:"status"`
+	Recipient             string      `json:"recipient"`
+	RequesterId           uint32      `json:"requester_id"`
+	SubmitterId           uint32      `json:"submitter_id"`
+	AssigneeId            uint32      `json:"assignee_id"`
+	OrganizationId        uint32      `json:"organization_id"`
+	GroupId               uint32      `json:"group_id"`
+	CollaboratorIds       []int32     `json:"collaborator_ids"`
+	ForumTopicId          uint32      `json:"forum_topic_id"`
+	ProblemId             uint32      `json:"problem_id"`
+	HasIncidents          bool        `json:"has_incidents"`
+	DueAt                 string      `json:"due_at"`
+	Tags                  []string    `json:"tags"`
+	Satisfaction_rating   string      `json:"satisfaction_rating"`
+	Ticket_form_id        uint32      `json:"ticket_form_id"`
+	Sharing_agreement_ids interface{} `json:"sharing_agreement_ids"`
+	Via                   interface{} `json:"via"`
+	Custom_Fields         interface{} `json:"custom_fields"`
+	Fields                interface{} `json:"fields"`
 }
 
 func (a Auth) ListTickets() (*Resource, error) {
