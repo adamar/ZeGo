9676172838eca3d9e04c8eb7e6076336580434cb
Josh <joshua.ms.cox@gmail.com>
2015-06-18 11:46:17 -0500

    Added structs for FieldValue and Via

--- zego/tickets.go
+++ zego/tickets.go
@@ -16,36 +16,52 @@ type SingleTicket struct {
 	Ticket *Ticket `json:"ticket"`
 }
 
+type FieldValue struct {
+	Id    uint64 `json:"id"`
+	Value string `json:"value"`
+}
+
+type Source struct {
+	From interface{} `json:"from"`
+	To   interface{} `json:"to"`
+	Rel  string      `json:"rel"`
+}
+
+type Via struct {
+	Channel string `json:"channel"`
+	Source  Source `json:"source"`
+}
+
 type Ticket struct {
-	Id                    uint64      `json:"id"`
-	URL                   string      `json:"url"`
-	ExternalId            string      `json:"external_id"`
-	CreatedAt             string      `json:"created_at"`
-	UpdatedAt             string      `json:"updated_at"`
-	Type                  string      `json:"type"`
-	Subject               string      `json:"subject"`
-	RawSubject            string      `json:"raw_subject"`
-	Description           string      `json:"description"`
-	Priority              string      `json:"priority"`
-	Status                string      `json:"status"`
-	Recipient             string      `json:"recipient"`
-	RequesterId           uint32      `json:"requester_id"`
-	SubmitterId           uint32      `json:"submitter_id"`
-	AssigneeId            uint32      `json:"assignee_id"`
-	OrganizationId        uint32      `json:"organization_id"`
-	GroupId               uint32      `json:"group_id"`
-	CollaboratorIds       []int32     `json:"collaborator_ids"`
-	ForumTopicId          uint32      `json:"forum_topic_id"`
-	ProblemId             uint32      `json:"problem_id"`
-	HasIncidents          bool        `json:"has_incidents"`
-	DueAt                 string      `json:"due_at"`
-	Tags                  []string    `json:"tags"`
-	Satisfaction_rating   string      `json:"satisfaction_rating"`
-	Ticket_form_id        uint32      `json:"ticket_form_id"`
-	Sharing_agreement_ids interface{} `json:"sharing_agreement_ids"`
-	Via                   interface{} `json:"via"`
-	Custom_Fields         interface{} `json:"custom_fields"`
-	Fields                interface{} `json:"fields"`
+	Id                    uint64       `json:"id"`
+	URL                   string       `json:"url"`
+	ExternalId            string       `json:"external_id"`
+	CreatedAt             string       `json:"created_at"`
+	UpdatedAt             string       `json:"updated_at"`
+	Type                  string       `json:"type"`
+	Subject               string       `json:"subject"`
+	RawSubject            string       `json:"raw_subject"`
+	Description           string       `json:"description"`
+	Priority              string       `json:"priority"`
+	Status                string       `json:"status"`
+	Recipient             string       `json:"recipient"`
+	RequesterId           uint32       `json:"requester_id"`
+	SubmitterId           uint32       `json:"submitter_id"`
+	AssigneeId            uint32       `json:"assignee_id"`
+	OrganizationId        uint32       `json:"organization_id"`
+	GroupId               uint32       `json:"group_id"`
+	CollaboratorIds       []int32      `json:"collaborator_ids"`
+	ForumTopicId          uint32       `json:"forum_topic_id"`
+	ProblemId             uint32       `json:"problem_id"`
+	HasIncidents          bool         `json:"has_incidents"`
+	DueAt                 string       `json:"due_at"`
+	Tags                  []string     `json:"tags"`
+	Satisfaction_rating   string       `json:"satisfaction_rating"`
+	Ticket_form_id        uint32       `json:"ticket_form_id"`
+	Sharing_agreement_ids interface{}  `json:"sharing_agreement_ids"`
+	Via                   Via          `json:"via"`
+	Custom_Fields         []FieldValue `json:"custom_fields"`
+	Fields                []FieldValue `json:"fields"`
 }
 
 func (a Auth) ListTickets(pag ...string) (*TicketArray, error) {
