f2fd53c8a383f23b98c7f73c5f804e357d6fc57f
ppop <paul.pop@hudl.com>
2015-07-22 14:09:17 +0100

    implemented ticket metrics API

---
+++ zego/ticketMetrics.go
@@ -0,0 +1,27 @@
+
+package zego
+
+//GET /api/v2/tickets/{ticket_id}/metrics.json
+
+type TicketMetric struct {
+    Id                 int `json:"id"`
+    Ticket_id          int `json:"ticket_id"`
+    Replies            int `json:"replies"`
+    ReplyTime          int `json:"reply_time_in_minutes.business"`
+    FirstResolution	   int `json:"first_resolution_time_in_minutes.business"`
+    CreatedAt          string `json:"created_at"`
+}
+
+
+func (a Auth) GetTicketMetrics(ticket_id int) (*Resource, error) {
+
+    path := "/tickets/" + string(ticket_id) + "/metrics.json"
+    resource, err := api(a, "GET", path, "")
+    if err != nil {
+        return nil, err
+    }
+    return resource, nil
+}
+
+
+
